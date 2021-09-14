8d363dc0fa369516b1062cc821823f1720e397aa
Jean Elie KUBEMBULA <jeaneliekubembula>
2021-09-14 01:08:26 +0100

    Ajout d'une activite principale dans le package adminitration

---
+++ app/src/main/java/cg/forum/askquestion/administration/Admin_Main_Activity.java
@@ -0,0 +1,16 @@
+package cg.forum.askquestion.administration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cg.forum.askquestion.R;
+
+public class Admin_Main_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_main);
+    }
+}
